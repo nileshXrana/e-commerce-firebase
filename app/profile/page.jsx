cca3019aef11a13a2f33942b3d3dff31a4f47a6f
Nilesh Rana <akhilrana7500@gmail.com>
2026-06-30 18:36:25 +0530

    base

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
@@ -8,6 +9,9 @@ import { useRouter } from "next/navigation";
 import Box from "@mui/material/Box";
 import "../ui/styles/profile.css";
 import RotateRightIcon from '@mui/icons-material/RotateRight';
+import ProfileTab from "../ui/ProfileTab";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 export default function ProfilePage() {
   const router = useRouter();
@@ -18,6 +22,13 @@ export default function ProfilePage() {
   const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
   const [message, setMessage] = useState({ text: "", type: "" });
+  const [activeTab, setActiveTab] = useState("general");
+
+  const [value, setValue] = React.useState('one');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -71,57 +82,136 @@ export default function ProfilePage() {
   if (loading) {
     return (
       <Box className="profile-container">
-        <RotateRightIcon className="infiniteLoader"/>
+        <RotateRightIcon className="infiniteLoader" />
       </Box>
     );
   }
 
   return (
-    <Box className="profile-container">
-      <Box className="profile-card">
-        <h1 className="profile-title">Your Profile</h1>
 
-        {message.text && (
-          <p className={`profile-message ${message.type}`}>
-            {message.text}
-          </p>
-        )}
-
-        {user && (
-          <form onSubmit={handleSave}>
-            <Box className="profile-info">
-              <label htmlFor="profile-name">Name</label>
-              <input
-                id="profile-name"
-                type="text"
-                className="profile-input"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Enter your name"
-                required
-              />
-            </Box>
-
-            <Box className="profile-info">
-              <label>Email address</label>
-              <p>{email}</p>
-            </Box>
-
-            <Box className="profile-info">
-              <label>Account Role</label>
-              <p style={{ textTransform: "capitalize" }}>{role}</p>
-            </Box>
-
-            <button
-              type="submit"
-              className="profile-button"
-              disabled={saving}
-            >
-              {saving ? "Saving..." : "Save Changes"}
-            </button>
-          </form>
-        )}
+    <Box>
+      <Box sx={{ width: '100%',}}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+        >
+          <Tab value="one" label="General" onClick={()=>setActiveTab("general")}/>
+          <Tab value="two" label="Address" onClick={() => setActiveTab("address")}/>
+          <Tab value="three" label="Privacy" onClick={() => setActiveTab("privacy")}/>  
+        </Tabs>
       </Box>
+
+      {/* <Box className="tabs-container">
+        <button
+          className={`profile-tab ${activeTab === "general" ? "active" : ""}`}
+          onClick={() => setActiveTab("general")}
+        >
+          General
+        </button>
+        <button
+          className={`profile-tab ${activeTab === "security" ? "active" : ""}`}
+          onClick={() => setActiveTab("address")}
+        >
+          Address
+        </button>
+        <button
+          className={`profile-tab ${activeTab === "security" ? "active" : ""}`}
+          onClick={() => setActiveTab("privacy")}
+        >
+          Privacy
+        </button>
+      </Box> */}
+
+      {activeTab === "general" && (
+        <Box className="profile-container">
+          <Box className="profile-card">
+            <h1 className="profile-title">Your Profile</h1>
+
+            {message.text && (
+              <p className={`profile-message ${message.type}`}>
+                {message.text}
+              </p>
+            )}
+
+            {user && (
+              <form onSubmit={handleSave}>
+                <Box className="profile-info">
+                  <label htmlFor="profile-name">Name</label>
+                  <input
+                    id="profile-name"
+                    type="text"
+                    className="profile-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter your name"
+                    required
+                  />
+                </Box>
+
+                <Box className="profile-info">
+                  <label>Email address</label>
+                  <p>{email}</p>
+                </Box>
+
+                <Box className="profile-info">
+                  <label>Account Role</label>
+                  <p style={{ textTransform: "capitalize" }}>{role}</p>
+                </Box>
+
+                <button
+                  type="submit"
+                  className="profile-button"
+                  disabled={saving}
+                >
+                  {saving ? "Saving..." : "Save Changes"}
+                </button>
+              </form>
+            )}
+          </Box>
+        </Box>
+      )}
+
+      {activeTab === "address" && (
+        <Box className="profile-container">
+          <Box className="profile-card">
+            <h1 className="profile-title">Saved Address</h1>
+
+            {user && (
+              <form onSubmit={handleSave}>
+                <Box className="profile-info">
+                  <label htmlFor="profile-name">Address</label>
+                  <input
+                    id="profile-name"
+                    type="text"
+                    className="profile-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter your name"
+                    required
+                  />
+                </Box>
+
+                <Box className="profile-info">
+                  <label>Account Role</label>
+                  <p style={{ textTransform: "capitalize" }}>{role}</p>
+                </Box>
+
+                <button
+                  type="submit"
+                  className="profile-button"
+                  disabled={saving}
+                >
+                  {saving ? "Saving..." : "Save Changes"}
+                </button>
+              </form>
+            )}
+          </Box>
+        </Box>
+      )}
+
     </Box>
   );
 }
